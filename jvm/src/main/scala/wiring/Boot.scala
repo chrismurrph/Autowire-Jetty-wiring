753b726400765150b8bb1dd53b3c2ba3b5fe5cd7
chris <chris@strandz.org>
2015-08-09 20:14:19 +1000

    Give the context of Boot.scala

--- jvm/src/main/scala/wiring/Boot.scala
+++ jvm/src/main/scala/wiring/Boot.scala
@@ -1,19 +1,15 @@
 package wiring
 
 import akka.actor.{Props, ActorSystem}
-// Keep generic until up on GitHub
-// import com.seasoft.server.applic.RemoteLocalActorConstants
-// import RemoteLocalActorConstants._
 import spray.servlet.WebBoot
 
-// this class is instantiated by the servlet initializer
-// it needs to have a default constructor and implement
-// the spray.servlet.WebBoot trait
+/**
+ * application.conf instantiates this (see WEB-INF/classes)
+ */ 
 class Boot extends WebBoot {
 
   val systemName = "WIRING_GITHUB_EXAMPLE"
   println("Now booting " + systemName)
-  // we need an ActorSystem to host our application in
   val system = ActorSystem(systemName)
 
   // the service actor replies to incoming HttpRequests
